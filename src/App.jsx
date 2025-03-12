e6382f5a7c37b0c3fbc81abaf44c4909e48505c8
Vignesh S <vigneshsmart1103@gmail.com>
2025-03-12 17:50:50 +0530

    created a demo project to learn and explore more about React useContext API to avoid props drilling also exploring React reducer

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import CartContextProvider from './Store/Shopping-cart-context.jsx';
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx'
+
+
+function App(){
+
+
+  return (
+    <CartContextProvider>
+      <Header/>
+      <Shop>
+      {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
+    </CartContextProvider>
+  );
+}
+
+export default App;
