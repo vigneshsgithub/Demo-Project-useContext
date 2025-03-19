c7fe5a265a262663b003ca55f3d664c9b3a19842
Vignesh S <vigneshsmart1103@gmail.com>
2025-03-19 09:41:16 +0530

    Learnt hooks and Props

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,29 @@ import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
 import Product from './components/Product.jsx'
+import { useReducer } from 'react';
 
+ const initialState={count:0};
+
+ function reducer(state,action){
+  switch(action.type){
+    case "increment":
+      return{count:state.count+1};
+    case "decrement":
+      return{count:state.count-1};
+    case "reset":
+      return{count:state.count=0};
+    default:
+      throw new Error('Unknown action type');
+    
+  }
+
+ }
+
+ 
 
 function App(){
+  const[state,dispatch]=useReducer(reducer,initialState);
 
 
   return (
@@ -19,6 +39,7 @@ function App(){
           </li>
         ))}
       </Shop>
+      
     </CartContextProvider>
   );
 }
